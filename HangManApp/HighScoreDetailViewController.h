f6e6f12e6bb7b1e22c2cdbc37b344e80ba773596
Kim Landeweer <krelandeweer@gmail.com>
2014-05-10 12:55:53 +0200

    added high score detail view controller and mockup image

---
+++ HangManApp/HighScoreDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  HighScoreDetailViewController.h
+//  HangManApp
+//
+//  Created by Kim on 10/05/14.
+//  Copyright (c) 2014 Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HighScoreDetailViewController : UIViewController
+
+@end
